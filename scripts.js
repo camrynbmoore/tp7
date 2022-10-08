e9a9764aefda5596837df5a7091686a2371c09bd
Camryn <camryn.moore@wsu.edu>
2022-10-08 00:16:41 -0400

    Finished requirements for my recipe. Started to add other recipes but did not finish.

---
+++ scripts.js
@@ -0,0 +1,87 @@
+//function to load a file from the URL "fromFile" into the object "whereTo"
+function loadFileInto(fromFile, whereTo) {
+
+  // creating a new XMLHttpRequest object
+  ajax = new XMLHttpRequest();
+
+  // defines the GET/POST method, source, and async value of the AJAX object
+  ajax.open("GET", fromFile, true);
+
+  // provides code to do something in response to the AJAX request
+  ajax.onreadystatechange = function() {
+    if ((this.readyState == 4) && (this.status == 200)) {
+      document.querySelector(whereTo).innerHTML = this.responseText;
+
+    } else if ((this.readyState == 4) && (this.status != 200)) {
+      console.log("Error: " + this.responseText);
+    }
+
+  } // end ajax.onreadystatechange function
+
+  // initiate request and wait for response
+  ajax.send();
+
+}
+
+// New receipe object- generic recipe creator
+function Recipe(recipeName, contributorName, imageURL, ingredientsURL, equipmentURL, directionsURL) {
+  
+  this.recipeName = recipeName;
+  this.contributor = contributorName;
+  this.imageURL = imageURL;
+  this.ingredients = ingredientsURL;
+  this.equipment = equipmentURL;
+  this.directions = directionsURL;
+  
+  this.displayRecipe = function() {
+    document.querySelector("#heroimage h1").innerHTML= this.recipeName;
+    document.querySelector("#contributor").innerHTML= this.contributor;
+    document.querySelector("#heroimage").style.backgroundImage = "url("+ this.imageURL + ")";
+    loadFileInto(this.ingredients, "#ingredients ul");
+    loadFileInto(this.equipment, "#equipment ul");
+    loadFileInto(this.directions, "#directions ol");
+  }
+  
+}
+
+
+TacoDip = new Recipe("Taco Dip", 
+                     "Camryn", 
+                     "https://images.unsplash.com/photo-1523634700860-90d0ef74f137?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1450&q=80", 
+                     "ingredients.html", 
+                     "equipment.html", 
+                     "directions.html"
+                    );
+
+
+CaramelFlan = new Recipe("Caramel Flan", 
+                     "Bert Adams", 
+                     "https://images.unsplash.com/photo-1639108094328-2b94a49b1c2e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80", 
+                     "caramelflan-ingredients.html", 
+                     "caramelflan-equipment.html", 
+                     "caramelflan-directions.html"
+                    );
+
+
+
+
+window.onload = function() {
+  
+  document.querySelector("#firstRecipe").onclick = function() {
+    TacoDip.displayRecipe();
+    
+  document.querySelector("secondRecipe").onclick = function() {
+    CaramelFlan.displayRecipe();
+  }
+    
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+}
